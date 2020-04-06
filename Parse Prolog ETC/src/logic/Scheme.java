714750768c51aeb0644ab63274b688bdd77f3f2b
Gabriel <20170388@ce.pucmm.edu.do>
2020-04-05 22:20:35 -0400

    implementacion de scheme

---
+++ Parse Prolog ETC/src/logic/Scheme.java
@@ -0,0 +1,25 @@
+package logic;
+
+import jscheme.JScheme;
+import jscheme.SchemeException;
+import java.io.*;
+
+public class Scheme {
+
+	public static float porcentajeGrasa(float peso, float altura, int edad, int genero)
+	{
+		JScheme js = null;
+		float resultado = -1;
+		try
+		{
+			js = new JScheme();
+			js.load(new FileReader("formulasNutricion.scsm"));
+		}catch (FileNotFoundException e) {
+			// TODO: handle exception
+			return resultado;
+		}
+		
+		resultado = (float) js.call("porcentajeGrasa", peso, altura, edad, genero);
+		return resultado;
+	}
+}
